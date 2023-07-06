6c263fb8dc4029c2b929c9cf34cae2e9784118c4
Matheus Rotta <matheusrotta7@gmail.com>
2023-07-06 10:38:04 -0300

    first commit with some tests

---
+++ pages/basics/first.jsx
@@ -0,0 +1,7 @@
+import '../../src/app/globals.css'
+
+export default function first() {
+    return (
+        <div className="text-red-500 bg-sky-200 hover:bg-sky-600"> primeiro</div>
+    )
+}
---
+++ pages/basics/second.jsx
@@ -0,0 +1,14 @@
+export default function second() {
+
+    const subtitle = "teste 123"
+
+    return (
+        <div>
+            <h1>Segundo</h1>
+            <h2>Hello, World!</h2>
+            <div className="bg-gray-500">yapper dapper</div>
+            <h3>{subtitle}</h3>
+            <h4>{Math.max(13, 39)}</h4>
+        </div>
+    )
+}
